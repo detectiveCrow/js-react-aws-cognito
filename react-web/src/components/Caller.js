4342576bb4bc5025c9d7f29208883b7138b0eae8
detectivecrow <detectivecrow2540@gmail.com>
2022-01-21 16:47:26 +0900

    Update caller UI

--- react-web/src/components/Caller.js
+++ react-web/src/components/Caller.js
@@ -2,7 +2,7 @@ import React, { useState, useContext } from "react";
 import { AccountContext } from './Accounts';
 import * as axios from "axios";
 
-import { Button, Card } from '@mui/material';
+import { Button, ButtonGroup, Card } from '@mui/material';
 
 const Caller = () => {
     const [token, setToken] = useState("");
@@ -24,7 +24,7 @@ const Caller = () => {
         })
           .then(res => {
             console.log(res)
-            setResult(res.data.body)
+            setResult(res.data)
           })
           .catch(error => {
             console.error(error)
@@ -33,19 +33,35 @@ const Caller = () => {
 
     return (
       <div>
-        <Button
-          onClick={onSubmit('/hello')}
-          variant="contained"
-        >
-          Test API Gateway
-        </Button>
+        <ButtonGroup size="small" aria-label="button group">
+          <Button
+            onClick={onSubmit('/hello')}
+            variant="contained"
+          >
+            /hello
+          </Button>
 
-        <Button
-          onClick={onSubmit('/quote')}
-          variant="contained"
-        >
-          Get Random Quote
-        </Button>
+          <Button
+            onClick={onSubmit('/quote')}
+            variant="contained"
+          >
+            /quote
+          </Button>
+
+          <Button
+            onClick={onSubmit('/numbers')}
+            variant="contained"
+          >
+            /numbers
+          </Button>
+
+          <Button
+            onClick={onSubmit('/auth')}
+            variant="contained"
+          >
+            /auth
+          </Button>
+        </ButtonGroup>
 
         <Card variant="outlined" sx={{ mt: 2, p: 3 }}>{result}</Card>
       </div>
